f4e5ed6d30f787ea6de00d644258581a63fecb1f
ManjuRamu <131784940+ManjuRamu@users.noreply.github.com>
2024-09-23 09:43:41 -0700

    sub

--- pattrens/prefix-sum/03-sub-arry-equal-k.js
+++ pattrens/prefix-sum/03-sub-arry-equal-k.js
@@ -3,9 +3,12 @@
  * @param {number} k
  * @return {number}
  */
+
+
+//brute-force
 var subarraySum = function (nums, k) {
     let output = 0;
-    for (let i = 0; i < nums.lenght; i++) {
+    for (let i = 0; i < nums.length ; i++) {
         let sum = nums[i];
         console.log({ sum, k })
         if (sum === k) output++;
@@ -19,3 +22,29 @@ var subarraySum = function (nums, k) {
     return output;
 };
 console.log(subarraySum([1,1,1],2))
+/*
+0,1,2,3,4
+
+0,1
+0,1,2
+0,1,2,3
+0,1,2,3,4
+
+1,2
+1,2,3
+1,2,3,4
+
+2,3
+2,3,4
+
+3,4
+
+*/
+
+function subarraySumPreSum(arr, k){
+    const prefixSum =[],count=0;
+    prefixSum[i] = arr[i];
+    for(let i= 1; i<arr.length; i++){
+        prefixSum[i+1] = prefixSum[i]+arr[i];
+    }
+}
