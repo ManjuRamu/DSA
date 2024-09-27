d96e16075abdff88e695c22073817caf0515137e
Manju <manju.r.learning@gmail.com>
2024-09-27 12:03:13 +0530

    #

--- recursion/decimal-to-binary.js
+++
@@ -1,8 +0,0 @@
-let num = 1;
-function decimalToBinary(num) {
-  if (num < 1) {
-    return num;
-  }
-  return decimalToBinary(parseInt(num / 2)) + String(num % 2);
-}
-console.log(decimalToBinary(num));
