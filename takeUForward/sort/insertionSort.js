576b9328d466fdd553ad6cbde7955eb51d20ddc2
ManjuRamu <manju.r.learning@gmail.com>
2025-04-15 23:18:41 +0530

    #V2 to remove not need swapping

--- takeUForward/sort/insertionSort.js
+++ takeUForward/sort/insertionSort.js
@@ -28,3 +28,18 @@ function insertionSort(){
     console.log(arr)
 }
 insertionSort()
+
+function insertionSortV2() {
+    const arr = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+    for (let i = 1; i < arr.length; i++) {
+      const current = arr[i];
+      let j = i - 1;
+      while (j >= 0 && arr[j] > current) {
+        arr[j + 1] = arr[j]; // shift right
+        j--;
+      }
+      arr[j + 1] = current; // place in correct position
+    }
+    console.log(arr);
+  }
+  
