93de5014106bd98c005fbd7c0348e0c24be8a15f
ManjuRamu <manju.r.learning@gmail.com>
2025-04-22 23:40:15 +0530

    clean

--- takeUForward/array/union.js
+++ takeUForward/array/union.js
@@ -1,31 +1,31 @@
-const a =[1,1,2,3,4,5,6,12, 14,15]
-const b= [1,2,4,4,4,4,6,6,7,8]
-const res = []
-let i=0,j=0;
-while(i < a.length && j <b.length){
-  if(a[i] <= b[j]){
-    if(res[res.length -1] != a[i]){
-        res.push(a[i])
-    }
-    i++
- }
- else {
-    if(res[res.length -1] != b[j]){
-        res.push(b[j])
-    }
-    j++
- }
+const a = [1, 1, 2, 3, 4, 5, 6, 12, 14, 15];
+const b = [1, 2, 4, 4, 4, 4, 6, 6, 7, 8];
+const res = [];
+let i = 0,
+  j = 0;
+while (i < a.length && j < b.length) {
+  let e;
+  if (a[i] <= b[j]) {
+    e = a[i];
+    i++;
+  } else {
+    e = b[j];
+    j++;
+  }
+  if (res[res.length - 1] != e) {
+    res.push(e);
+  }
 }
-while( i < a.length){
-    if(res[res.length -1] != a[i]){
-        res.push(a[i])
-    }
-    i++
+while (i < a.length) {
+  if (res[res.length - 1] != a[i]) {
+    res.push(a[i]);
+  }
+  i++;
 }
-while( j < b.length){
-    if(res[res.length -1] != b[j]){
-        res.push(b[j])
-    }
-    j++
+while (j < b.length) {
+  if (res[res.length - 1] != b[j]) {
+    res.push(b[j]);
+  }
+  j++;
 }
-console.log(res)
+console.log(res);
